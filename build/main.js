6b423f98be6d37558f7f32ec494bc755049da5c7
Anderw Medvedev <medvedev32134@gmail.com>
2022-10-07 03:25:43 +0500

    jsfunction_commit

--- build/main.js
+++ build/main.js
@@ -6,14 +6,57 @@ totalCost += buttonItem.querySelector('.input').value * buttonItem.querySelector
 
 });
 
-document.getElementById('total-h').textContent = totalCost;
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+}
 
-};
+clear() {
+}
 
-init();
+delete() {
+}
 
-let button = document.getElementById(".button__1h")[0];
+appendNumber(number) {
+}
 
-elementToDetect[0].addEventListener("click", function(){
-  this.style.visibility = "hidden";
-});
+chooseOperation(operation) {
+}
+
+compute() {
+}
+
+
+
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+
+})
+
+appendNumber(number) {
+  if (number === '.' && this.currentOperand.includes('.')) return
+  this.currentOperand = this.currentOperand.toString() + number.toString()
+}
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+chooseOperation(operation) {
+  if (this.currentOperand === '') return
+  if (this.previousOperand !== '') {
+    this.compute()
+  }
+  this.operation = operation
+  this.previousOperand = this.currentOperand
+  this.currentOperand = ''
+}
